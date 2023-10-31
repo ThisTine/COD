d958613d689d86584a67c0e0e74368eda720d096
Pn41e <98150552+Pn41e@users.noreply.github.com>
2023-10-31 18:18:24 +0700

    Update predict_object.py
    remove people and other unnecessary classes

--- utils/object_position/predict_object.py
+++ utils/object_position/predict_object.py
@@ -6,4 +6,19 @@ class ObjectModel:
         self.model = YOLO("weight/object/openImg.pt")
 
     def predict(self, source):
-        return self.model.predict(source=source,stream=True, conf=0.2)
+        includeClass = [*range(0,601)]
+        for i in range(259, 272):
+            print(i)
+            includeClass.remove(i)
+        includeClass.remove(70)
+        includeClass.remove(115)
+        includeClass.remove(116)
+        includeClass.remove(217)
+        includeClass.remove(283)
+        includeClass.remove(322)
+        includeClass.remove(354)
+        includeClass.remove(381)
+        includeClass.remove(505)
+        includeClass.remove(594)
+        
+        return self.model.predict(source=source,stream=True, conf=0.2, hide_labels=True, classes=includeClass)
