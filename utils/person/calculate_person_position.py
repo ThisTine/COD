a53fe4a6782f44c67bb441c1d840a57e9b411037
BlueBox <pongsapukanuvech@gmail.com>
2023-11-29 18:07:47 +0700

    [ADD] HTTP REQ

--- utils/person/calculate_person_position.py
+++ utils/person/calculate_person_position.py
@@ -1,3 +1,5 @@
+import  requests
+import ssl
 def calculate_person_position(bigBox: (int,int), smallBox: (int,int)):
     b_x1 = bigBox[0]
     b_x2 = bigBox[1]
@@ -6,18 +8,35 @@ def calculate_person_position(bigBox: (int,int), smallBox: (int,int)):
 
     left = s_x1 - b_x1
     right = b_x2 - s_x2
-
+    # response = Request('GET', "http://cod9000.pspgun.com/")
+    # prepped = s.prepare_request(response)
+    # resp = s.send(prepped)
+    # print(resp.status_code)
+    # print(response)
+    # print(prepped)
+    print(requests.get("https://httpbin.org/get"))
     if left < 0 < right:
+        response = requests.patch("https://cod9000.pspgun.com/change/L",verify=False)
+        print(response.status_code)
         return "LEFT"
     if right < 0 < left:
+        response = requests.patch("https://cod9000.pspgun.com/change/R",verify=False)
+        print(response.status_code)
         return "RIGHT"
     if left >= 0 and right >= 0 :
+        response = requests.patch("https://cod9000.pspgun.com/change/S",verify=False)
+        print(response.status_code)
         return "CENTER"
 
     if abs(left) - abs(right) > 0 :
+        response = requests.patch("https://cod9000.pspgun.com/change/L",verify=False)
+        print(response.status_code)
         return "LEFT"
     if abs(right) - abs(left) > 0 :
+        response = requests.patch("https://cod9000.pspgun.com/change/R",verify=False)
+        print(response.status_code)
         return "RIGHT"
+    response = requests.post("https://cod9000.pspgun.com/change/S",verify=False)
     return "CENTER"
 
-    return "LEFT"
+    # return "LEFT"
